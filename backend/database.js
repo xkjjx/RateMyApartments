62e55f56cf6454ba474bad84bf8337485f39236f
Kevin Johnson <john2003.kjj@gmail.com>
2024-04-11 21:03:25 -0500

    added some backend functions

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,62 @@
-const { response } = require('express');
-
+const { response, json } = require('express')
 const Pool = require('pg').Pool
 require('dotenv').config()
+
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    database: 'ratemyapartment',
+    user: 'kjj',
+    password: "npc"
+})
+
+const closePool = () => {
+    pool.end();
+}
+
+module.exports = { pool, closePool }
+
+const addArea = (request, response) => {
+    const name = request.body.name;
+
+    pool.query('INSERT INTO area (name) VALUES ($1)', [name], (error, results) => {
+        if (error) {
+            response.status(400).send(`Error: ${error}`);
+        }
+        response.status(201).send(`Area added with ID: ${results.insertId}`);
+    })
+}
+
+const getAreas = (request, response) => {
+    pool.query('SELECT * FROM area', (error, results) => {
+        if (error) {
+            response.status(400).send(`Error: ${error}`);
+        }
+        response.status(200).json(results.rows);
+    })
+}
+
+const getApartmentsInArea = (request, response) => {
+    const areaId = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM area_apartments WHERE area_id = $1', [areaId], (error, results) => {
+        if (error) {
+            response.status(400).send(`Error: ${error}`);
+        }
+        const apartmentId = parseInt(results.rows[0].apartment_id);
+        console.log(apartmentId);
+        pool.query('SELECT * FROM apartments WHERE id = $1', [apartmentId], (error, results) => {
+            if (error) {
+                response.status(400).send(`Error: ${error}`);
+            }
+            response.status(200).json(results.rows);
+        })
+    })
+}
+
+
+module.exports = { addArea, getAreas, getApartmentsInArea }
+
+
+
+
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const db = require('./database.js')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
 
 require('dotenv').config()
+
+process.on("SIGINT", () => {
+    db.closePool()
+    process.exit()
+    }
+);
 
 
 app.get('/', (req, res) => {
@@ -12,3 +23,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post('/areas', db.addArea)
+app.get('/areas', db.getAreas)
+app.get('/areas/:id/apartments', db.getApartmentsInArea)
